a93aa360abe00c1c1a47daf0be7eafa80f62ce9a
Devoxin <lukel9914@gmail.com>
2017-10-14 19:07:23 +0100

    Fix undefined version

--- commands/stats.js
+++ commands/stats.js
@@ -10,7 +10,7 @@ module.exports = {
 				{ name: "RAM Usage",  value: `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`, inline: true },
 				{ name: "Ping",       value: `${ctx.client.pings[0]} ms`,							       inline: true },
 				{ name: "Node",       value: process.version, 											   inline: true },
-				{ name: "Discord.JS", value: ctx.Discord.version,  								     	   inline: true },
+				{ name: "Discord.JS", value: ctx.client.Discord.version,  								   inline: true },
 				{ name: "Servers",    value: ctx.client.guilds.size, 								       inline: true }
 			]
 		}});
