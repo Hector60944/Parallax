fe05fc34bbc231ba0c9eb7198214feb3400fae23
Devoxin <lukel9914@gmail.com>
2018-01-30 12:30:36 +0000

    Bug hunting

--- extensions/timedwatch.py
+++ extensions/timedwatch.py
@@ -36,77 +36,83 @@ class Watcher:
             mutes = await self.r.table('mutes').run(self.bot.connection)
 
             for entry in bans.items:
-                if int(entry['due']) > now:
-                    continue
-
-                guild = self.bot.get_guild(int(entry['guild_id']))
-                user = await self.resolve_user(int(entry['user_id']))
-
-                if not guild or not guild.me.guild_permissions.ban_members:
-                    continue
-
                 try:
-                    await guild.unban(discord.Object(id=int(entry['user_id'])), reason='[ Auto-Unban ] Expired')
-                except (discord.HTTPException, discord.Forbidden):  # !?!?
-                    pass
-                else:
-                    await self.remove_entry(guild.id, user.id, 'bans')
-                    db = await self.r.table('settings').get(str(guild.id)).run(self.bot.connection)
-
-                    if not db or not db['logChannel']:
+                    if int(entry['due']) > now:
                         continue
 
-                    channel = self.bot.get_channel(int(db['logChannel']))
-                    if channel:
-                        permissions = channel.permissions_for(guild.me)
-                        if permissions.send_messages and permissions.embed_links:
-                            embed = discord.Embed(color=0xbe2f2f,
-                                                  title=f'**User Unbanned**',
-                                                  description=f'**Target:** {str(user)} ({user.id})\n'
-                                                              f'**Reason:** [ Auto-Unban ] Expired',
-                                                  timestamp=datetime.utcnow())
-                            embed.set_footer(text=f'Performed by {self.bot.user}', icon_url=self.bot.user.avatar_url_as(format='png'))
+                    guild = self.bot.get_guild(int(entry['guild_id']))
+                    user = await self.resolve_user(int(entry['user_id']))
 
-                            await channel.send(embed=embed)
+                    if not guild or not guild.me.guild_permissions.ban_members:
+                        continue
+
+                    try:
+                        await guild.unban(discord.Object(id=user.id), reason='[ Auto-Unban ] Expired')
+                    except (discord.HTTPException, discord.Forbidden):  # !?!?
+                        pass
+                    else:
+                        await self.remove_entry(guild.id, user.id, 'bans')
+                        db = await self.r.table('settings').get(str(guild.id)).run(self.bot.connection)
+
+                        if not db or not db['logChannel']:
+                            continue
+
+                        channel = self.bot.get_channel(int(db['logChannel']))
+                        if channel:
+                            permissions = channel.permissions_for(guild.me)
+                            if permissions.send_messages and permissions.embed_links:
+                                embed = discord.Embed(color=0xbe2f2f,
+                                                    title=f'**User Unbanned**',
+                                                    description=f'**Target:** {str(user)} ({user.id})\n'
+                                                                f'**Reason:** [ Auto-Unban ] Expired',
+                                                    timestamp=datetime.utcnow())
+                                embed.set_footer(text=f'Performed by {self.bot.user}', icon_url=self.bot.user.avatar_url_as(format='png'))
+
+                                await channel.send(embed=embed)
+                except Exception as e:
+                    print(e)
 
             for entry in mutes.items:
-                if int(entry['due']) > now:
-                    continue
-
-                guild = self.bot.get_guild(int(entry['guild_id']))
-                user = await self.resolve_user(int(entry['user_id']))
-
-                if not guild or not guild.me.guild_permissions.manage_roles:
-                    continue
-
-                db = await self.r.table('settings').get(str(guild.id)).run(self.bot.connection)
-                member = guild.get_member(user.id)
-
-                if not db or not member or not db['mutedRole']:
-                    continue
-
-                await self.remove_entry(guild.id, user.id, 'mutes')
-
                 try:
-                    await member.remove_roles(discord.Object(id=int(entry['role_id'])), reason='[ Auto-Unmute ] Expired')
-                except (discord.HTTPException, discord.Forbidden):  # !?!?
-                    pass
-                else:
-                    if not db['logChannel']:
+                    if int(entry['due']) > now:
                         continue
 
-                    channel = self.bot.get_channel(int(db['logChannel']))
-                    if channel:
-                        permissions = channel.permissions_for(guild.me)
-                        if permissions.send_messages and permissions.embed_links:
-                            embed = discord.Embed(color=0xbe2f2f,
-                                                  title=f'**User Unmuted**',
-                                                  description=f'**Target:** {str(user)} ({user.id})\n'
-                                                              f'**Reason:** [ Auto-Unmute ] Expired',
-                                                  timestamp=datetime.utcnow())
-                            embed.set_footer(text=f'Performed by {self.bot.user}', icon_url=self.bot.user.avatar_url_as(format='png'))
+                    guild = self.bot.get_guild(int(entry['guild_id']))
+                    user = await self.resolve_user(int(entry['user_id']))
 
-                            await channel.send(embed=embed)
+                    if not guild or not guild.me.guild_permissions.manage_roles:
+                        continue
+
+                    db = await self.r.table('settings').get(str(guild.id)).run(self.bot.connection)
+                    member = guild.get_member(user.id)
+
+                    if not db or not member or not db['mutedRole']:
+                        continue
+
+                    await self.remove_entry(guild.id, user.id, 'mutes')
+
+                    try:
+                        await member.remove_roles(discord.Object(id=int(entry['role_id'])), reason='[ Auto-Unmute ] Expired')
+                    except (discord.HTTPException, discord.Forbidden):  # !?!?
+                        pass
+                    else:
+                        if not db['logChannel']:
+                            continue
+
+                        channel = self.bot.get_channel(int(db['logChannel']))
+                        if channel:
+                            permissions = channel.permissions_for(guild.me)
+                            if permissions.send_messages and permissions.embed_links:
+                                embed = discord.Embed(color=0xbe2f2f,
+                                                    title=f'**User Unmuted**',
+                                                    description=f'**Target:** {str(user)} ({user.id})\n'
+                                                                f'**Reason:** [ Auto-Unmute ] Expired',
+                                                    timestamp=datetime.utcnow())
+                                embed.set_footer(text=f'Performed by {self.bot.user}', icon_url=self.bot.user.avatar_url_as(format='png'))
+
+                                await channel.send(embed=embed)
+                except Exception as e:
+                    print(e)
 
             bans.close()
             mutes.close()
