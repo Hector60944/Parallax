72424c9b977482295ce528d41aca1375d5c3a566
Luke <dev@crimsonxv.pro>
2018-03-07 01:37:36 +0000

    Update moderation.py

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -332,7 +332,7 @@ class Moderation:
                     continue
                 r_name = r.name[:11] + '...' if len(r.name) > 11 else r.name
                 public_roles += f'{pad(r_name, " ", 15)}({r.id})\n'
-            await ctx.send(f'```\n{public_roles.strip()}\n```')
+            await ctx.send(f'```\n{public_roles.strip() or "No roles available"}\n```')
         else:
             if str(role.id) not in config['selfrole']:
                 return await ctx.send('That role isn\'t self-assignable')
