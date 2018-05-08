9c46c075848a611b4d59cf4a3d1afaa7ade114bf
Devoxin <lukel9914@gmail.com>
2018-05-08 15:52:21 +0100

    Mass reloading

--- extensions/core.py
+++ extensions/core.py
@@ -7,14 +7,21 @@ class Core:
 
     @commands.command()
     @commands.is_owner()
-    async def reload(self, ctx, module: str):
+    async def reload(self, ctx, *modules: str):
         """ Reload an extension """
-        try:
-            self.bot.unload_extension(f'extensions.{module}')
-            self.bot.load_extension(f'extensions.{module}')
-            await ctx.send('Module reloaded!')
-        except (SyntaxError, ModuleNotFoundError) as exception:
-            await ctx.send(f'Module **{module}** failed to load: `{exception}`')
+        failed = []
+        for module in modules:
+            try:
+                self.bot.unload_extension(f'extensions.{module}')
+                self.bot.load_extension(f'extensions.{module}')
+            except (SyntaxError, ModuleNotFoundError) as exception:
+                failed.append((module, str(exception)))
+
+        if failed:
+            formatted = '\n'.join([f'{f[0]:15} - {f[1]}' for f in failed])
+            await ctx.send(f'**{len(failed)}** extensions failed to reload\n```\n{formatted}```')
+        else:
+            await ctx.send('All modules reloaded successfully.')
 
     @commands.command()
     @commands.is_owner()
